caf4d4f4ae402a525561d28acbb941d1c19dfaa1
Leonardo Cesar <guy.fawkes.05111605.e@gmail.com>
2018-05-29 00:44:15 -0300

    Thrust and rotation control levers added. Trust and rotation speeds fine tuned.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour 
 {
+    [SerializeField] private float rcsThrust = 100f;
+    [SerializeField] private float mainThrust = 100f;
+    [SerializeField] private float upThrust = 100f;
 
     private Rigidbody rigidBody;
     private AudioSource audioSource;
@@ -19,15 +22,24 @@ public class Rocket : MonoBehaviour
 	// Update is called once per frame
 	void Update() 
 	{
-        ProcessInput();
+        Thrust();
+        Rotate();
 	}
 
-    private void ProcessInput()
+    private void Thrust()
     {
         // Check if it's being thrusted upwards
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rigidBody.AddRelativeForce(Vector3.right * mainThrust);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                rigidBody.AddForce(Vector3.up * upThrust);
+            }
+                
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -40,15 +52,25 @@ public class Rocket : MonoBehaviour
                 audioSource.Stop();
             }
         }
+    }
 
+    private void Rotate()
+    {
+        // Take manual control of rotation
+        rigidBody.freezeRotation = true;
+        
         // Check if it's being rotated
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * Time.deltaTime * rcsThrust);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.back);
+            transform.Rotate(Vector3.back * Time.deltaTime * rcsThrust);
         }
+
+        // Resume physics control of rotation
+        rigidBody.freezeRotation = false;
     }
+
 }
